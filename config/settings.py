58ac5ebfe42674b379a23fbd4458601d1c211e45
kyc3550 <rmawkxkq0909@naver.com>
2020-08-02 19:27:25 +0900

    make Views of coupon

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.naver',
     'cart',
+    'coupon',
 ]
 
 MIDDLEWARE = [
---
+++ coupon/urls.py
@@ -0,0 +1,9 @@
+from django import path
+
+from .views import add_coupon
+app_name = 'coupon'
+
+urlpatterns = [
+    path('add/', add_coupon, name='add'),
+    
+]
