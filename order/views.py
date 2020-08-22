7d254bbedf9a6d00c7726076682ca97d4c6dccb7
kyc3550 <rmawkxkq0909@naver.com>
2020-08-22 21:02:29 +0900

    OrderCheckoutAjaxView

--- order/views.py
+++ order/views.py
@@ -12,11 +12,11 @@ def order_create(request):
         # 입력받은 정보를 후처리
         form = OrderCreateForm(request.POST)
         if form.is_valid():
-            order = form.save()
+            order = form.save(commit=False)
             if cart.coupon:
                 order.coupon = cart.coupon
                 order.discount = cart.coupon.amount
-                order.save()
+            order.save()
             for item in cart:
                 OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
             cart.clear()
@@ -32,3 +32,55 @@ def order_complete(request):
     order_id = request.GET.get('order_id')
     order = Order.objects.get(id=order_id)
     return render(request,'order/created.html',{'order':order})
+
+from django.views.generic.base import View
+from django.http import JsonResponse
+
+class OrderCreateAjaxView(View):
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsoneResponse({"authenticated":False}, status=403)
+
+        cart = Cart(request)
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            if cart.coupon:
+                order.coupon = cart.coupon
+                order.discount = cart.coupon.amount
+                order.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+            cart.clear()
+            data = {
+                "order_id":order.id
+            }
+            return JsonResponse(data)
+        else:
+            return JsonResponse({}, status=401)
+            
+class OrderCheckoutAjaxView(View): 
+     def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({"authenticated":False}, status=403)
+
+        order_id = request.POST.get('order_id')
+        order = Order.objects.get(id=order_id)
+        amount = request.POST.get('amount')
+
+        try:
+            merchant_order_id = OrderTransaction.objects.create_new(
+                order=order,
+                amount=amount
+            )
+        except:
+            merchant_order_id = None
+        
+        if merchant_order_id is not None:
+            data = {
+                "works": True,
+                "merchant_id": merchant_order_id
+            }
+            return JsonResponse(data)
+        else:
+            return JsonResponse({}, status=401)
